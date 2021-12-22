d48aec5c7fedea73211fb8660cf4fd223f44546f
Eddy <sunmin9498@gmail.com>
2021-12-22 17:52:32 +0900

    first python

---
+++ coursera/first01.py
@@ -0,0 +1,54 @@
+# print(type("bbb"))
+# print(type(13))
+# print(type(1.55))
+
+
+# # Variables
+# length = 100
+# width = 3
+# area = length * width
+# print(area)
+ 
+
+
+# print(7+8.5)
+# print("a"+"b"+"c")
+# print("this "+ "is "+ "good " + "Eddy!!")
+# Variables 변수
+# Values
+# Expressions 표현
+# Explicit conversion (명시적 변환)
+
+# base = 6
+# height = 3
+# area = (base*height)/2
+# print("The area of the triangle is : " + str(area))
+
+
+# Difining Functions
+
+# def greeting(name):
+#     print("welcome, " + name)
+
+# greeting("Eddy")
+# greeting(str(455))
+# greeting("Twosome")
+
+# def greeting(name, departmant):
+#     print("Wellcome, " + name)
+#     print("You are part of " + departmant)
+
+# greeting("Eddy", "Seoul")
+# greeting("Black", "Software Engineering")
+
+# def print_seconds(hours, minutes, seconds):
+#     print(hours, minutes, seconds)
+
+# print_seconds(3600, 60, 1)
+
+def greeting(name, menu, when):
+    print("user name is " + name)
+    print("My favorite is " + menu)
+    print("why " + when)
+
+greeting("Eddy", "Kimchi", "beacause")
